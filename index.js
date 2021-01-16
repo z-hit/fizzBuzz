b0cd25fabae861d6901e15e374984c20c4f7d448
Zhenya Hitrova <60961500+z-hit@users.noreply.github.com>
2021-01-16 22:58:47 +1000

    Create index.js

---
+++ index.js
@@ -0,0 +1,21 @@
+var output = [];
+var count = 1;
+
+    function fizzBuzz() {
+    
+    while(count <= 100) {
+    if(count % 3 === 0 && count % 5 === 0) {
+        output.push("fizzBuzz");
+    } else if(count % 3 === 0) {
+        output.push("fizz");
+    } else if(count % 5 === 0) {
+        output.push("buzz");
+    } else {
+        output.push(count);
+    }
+
+    count++;
+    }
+    console.log(output);
+ 
+}
