3185bc0e43ae65341c8b7da07877fb2ff0d5b270
Zhenya Hitrova <60961500+z-hit@users.noreply.github.com>
2021-01-19 22:37:09 +1000

    added FOR loop

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 var output = [];
-var count = 1;
 
     function fizzBuzz() {
-    
-    while(count <= 100) {
+    for( var count = 1; count <= 100; count++) {
     if(count % 3 === 0 && count % 5 === 0) {
         output.push("fizzBuzz");
     } else if(count % 3 === 0) {
@@ -13,8 +11,6 @@ var count = 1;
     } else {
         output.push(count);
     }
-
-    count++;
     }
     console.log(output);
  
